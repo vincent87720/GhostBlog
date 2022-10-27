f2d0788a8a84d3e9fa955cb51a0a152b25533117
vincent87720 <vincent87720@yahoo.com.tw>
2022-10-28 01:46:17 +0800

    Feat: Added utterances section

--- Export.sh
+++ Export.sh
@@ -2,13 +2,13 @@
 wget --mirror --convert-links --page-requisites -nH --no-parent -P docs http://localhost:2368/
 
 #移除包含奇怪字串的style.css和script.js檔案
-sudo find ./docs -type f -name 'style.css?*' -exec rm {} \;
-sudo find ./docs -type f -name 'script.js?*' -exec rm {} \;
-sudo find ./docs/assets/font -type f -name '*?*' -exec rm {} \;
+find ./docs -type f -name 'style.css?*' -exec rm {} \;
+find ./docs -type f -name 'script.js?*' -exec rm {} \;
+find ./docs/assets/font -type f -name '*?*' -exec rm {} \;
 
 	
 #取代檔案內的奇怪字串
-sudo find ./docs -type f -name '*.html' -exec ./replace.pl {} \;
+find ./docs -type f -name '*.html' -exec ./replace.pl {} \;
 
 #複製assets到docs裡面
 cp -Rf content/themes/attila-3.1.1/assets/* docs/assets/

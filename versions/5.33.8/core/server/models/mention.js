bbc665adefdf454ff6bdab4d911654bd924fa4ad
vincent87720 <vincent87720@yahoo.com.tw>
2023-04-04 19:15:58 +0800

    Feat: remove version folders

--- versions/5.33.8/core/server/models/mention.js
+++
@@ -1,9 +0,0 @@
-const ghostBookshelf = require('./base');
-
-const Mention = ghostBookshelf.Model.extend({
-    tableName: 'mentions'
-});
-
-module.exports = {
-    Mention: ghostBookshelf.model('Mention', Mention)
-};
